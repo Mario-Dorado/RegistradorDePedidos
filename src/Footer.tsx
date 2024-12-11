af352e438be726e1c9fc532e760eb3cd10cb6c37
Mario <a2021115585@estudiantes.upsa.edu.bo>
2024-12-11 18:33:31 -0400

    chore: cambiado el contenido del Footer y el tamaño

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const Footer: React.FC = () => (
-  <footer className='bg-gradient-to-r from-pink-500 to-purple-300 py-4'>
+  <footer className='bg-gradient-to-r from-pink-500 to-purple-300 py-3'>
     <div className='text-center'>
-      <p>Con 💘 por Mario</p>
-      <p>© Todos los derechos reservados</p>
+      <p>Con 💘 por Marito para Lulita</p>
     </div>
   </footer>
 );
